92178525713c41b280f25abdafdbf17bc72b3d87
macong0503 <1512346520@qq.com>
2021-07-03 09:32:54 +0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,22 @@
+package ¸³ÖµÔËËã·û;
+
+public class Test {
+
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=10-2;
+		
+		//+= -= *= /=
+		
+		short s = 1;
+		s = (short)(s + 1);//short+int-->int+int
+
+		short s1 = 1;
+		s += 1;//s=(short)(s1+1)
+	
+		}
+	}
+
+}
